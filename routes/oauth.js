77d4650635ccad82decd9794cd8ef8d0000cc6a5
Guillaume Lerda <guillaume.lerda@busit.com>
2019-09-07 21:34:58 +0200

    oauth/token alias

--- routes/oauth.js
+++ routes/oauth.js
@@ -5,19 +5,37 @@ module.exports = (app, db) =>
 {
 	let OauthRouter = express.Router();
 
-	OauthRouter.route('/token').all(async (req, res, next) =>
-	{
-		let request = new oauthServer.Request(req);
-		let response = new oauthServer.Response(res);
+	OauthRouter.route('/token').all(
+		app.handlers.check_params({
+			mail: {
+				required: true,
+				match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+			},
+			pass: {
+				required: true,
+				min_length: 6,
+				max_length: 30
+			}
+		}),
+		(req, res, next) =>
+		{
+			req.body.username = req.body.mail;
+			req.body.password = req.body.pass;
+			next();
+		},
+		async (req, res) =>
+		{
+			let request = new oauthServer.Request(req);
+			let response = new oauthServer.Response(res);
 
-		let token = null;
-		try { token = await app.oauth.token(request, response); }
-		catch (err) {res.status(err.statusCode); res.json(app.createError(err.message)); return;}
-		token.client = undefined;
-		token.user = undefined;
+			let token = null;
+			try { token = await app.oauth.token(request, response); }
+			catch (err) { res.status(err.statusCode); res.json(app.createError(err.message)); return; }
+			token.client = undefined;
+			token.user = undefined;
 
-		res.json(app.createResponse(token));
-	});
+			res.json(app.createResponse(token));
+		});
 
 	return OauthRouter;
 };
